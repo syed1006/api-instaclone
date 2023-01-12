a4d407b1ff4ce1249b1533897c80d9fb42b23af6
Syed Salman <syedsalman1006@gmail.com>
2023-01-12 22:03:23 +0530

    basic server and database setup is completed

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const upload = require('./middleware/handleImages')
+
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Import routes
+const postRoute = require('./routes/blog');
+const userRoute = require('./routes/user');
+
+
+//Router MIddlewares
+const fetchUser = require('./middleware/fetchUser');
+app.use('/', fetchUser, postRoute);
+app.use('/', userRoute);
+
+module.exports = app;
